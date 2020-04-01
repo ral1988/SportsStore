0177cc01f7a39c7e224559da365b0233a69a16e3
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-04-01 10:32:01 -0400

    SportsStore Lab Notes for Visual Studio 2019/ASP.NET Core 2.2, 3.0, and 3.1 2.2 Modifications Copy/pasting from the book can introduce hidden characters which break code. BrowserLink requires Microsoft.VisualStudio.Web.BrowserLink package Ignore DotNetCliToolReference, pg 209 You must add Microsoft.AspNetCore.Mvc.ViewFeatures from Nuget for unit tests to compile, pg 219-220 Installing the Bootstrap Package, pg 231: Bower is "no longer supported". The current way to install client-side packages in Visual Studio is with "Library Manager" aka "libman". Manually create wwwroot folder. Note that it will change location (moves to the spot under "Properties") and icon. Create "lib" folder under "wwwroot" Right click on "lib" folder just created, choose "Add" > "Client-side Library..." Search for the "twitter-bootstrap@4.0.0-alpha6" library and install all files. Ensure you install the correct version; later versions have significantly different class names. Since we installed with libman rather than bower, the paths to link the stylesheets are different. use: <link rel="stylesheet"       asp-href-include="/lib/twitter-bootstrap/css/*.min.css"       asp-href-exclude="**/*-reboot*,**/*-grid*" /> instead of the listing on pg. 232. Replace @Html.Partial("ProductSummary", p) call with <partial name="ProductSummary" model="p" />, pg 235 Use Assert.Empty(<collection>) rather than Assert.Equal(0, <collection>.Count()) when ensuring an empty collection, pg 260, 261 ISesson.GetJson() and ISession.SetJson() methods are defined after being used, pg 265 Adding the Font Awesome Package, pg 276: Right click on the "lib" folder under "wwwroot", choose "Add" > "Client-side Library..." Install all files for the "font-awesome@4.7.0" As with bootstrap, the asp-href-include path is different: /lib/font-awesome/css/*.css, pg 278 Ensure the correct asp-href-include path when building _AdminLayout.cshtml, pg 298 Jquery, pg 316: Install as with Bootstrap and Font Awesome. Package names are "as expected", but ensure selection of correct versions as listed in the book. Ensure correct script paths based on jquery install paths, pg 316 After creating AppIdentityDbContext.cs, pg 324, modify ApplicationDbContext.cs, changing the constructor signature to public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) Optional: You can use <vc:cart-summary /> and comparable to invoke the view components, so long as you @addTagHelper *, SportsStore in the _ViewImports.cshtml. Adding just the Components namespace is not sufficient. Adding * does obviate the need for any other addTagHelper lines from SportsStore. Add the [Authorize] attribute to SeedDatabase() action method, pg 343 Use implicit typing wherever possible. 3.0 modifications Most of the packages are no longer needed in the web application. You must add ".AddNewtonSoftJson()" after ".AddMvc()" when configuring services. I did try to rewrite the Json serializing with the new, included System.Text.Json library, but couldn't get it to deserialize successfully. You must add "app.UseRouting();" after "UseStaticFiles", but before "UseAuthentication". You must add "app.UseAuthorization();" after "UseAuthentication()". They used to be combined, but are no longer. "UseMvc" becomes "UseEndpoints", "MapRoute" becomes "MapControllerRoute", and "template:" becomes "pattern:". 3.1 Modifications Ensure you have installed the 3.1.1 (or latest) versions of the following: Microsoft.AspNetCore.Identity.EntityFrameworkCore Microsoft.AspNetCore.Mvc.NewtonsoftJson Microsoft.EntityFrameworkCore.SqlServer Microsoft.EntityFrameworkCore.Tools

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace SportsStore
 {
@@ -24,7 +25,11 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc(routes => { });
+            app.UseRouting();
+            app.UseEndpoints(routes =>
+            {
+
+            });
         }
     }
 }

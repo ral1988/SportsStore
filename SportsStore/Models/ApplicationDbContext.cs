a5022cde2b9ae14ad20d65e21a96b107c05c7086
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-04-02 10:18:42 -0400

    added connection string to appsettings.json file

--- SportsStore/Models/ApplicationDbContext.cs
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -6,7 +6,8 @@ namespace SportsStore.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) {}
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
 
         public DbSet<Product> Products { get; set; }
     }

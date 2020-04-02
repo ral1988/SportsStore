abf0cdeb9eef207926e7138b5f4378ef59b3f77d
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-04-02 09:12:20 -0400

    Set the default route

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -30,7 +30,9 @@ namespace SportsStore
             app.UseRouting();
             app.UseEndpoints(routes =>
             {
-
+                routes.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller = Product}/{action = List}/{id ?}");
             });
         }
     }

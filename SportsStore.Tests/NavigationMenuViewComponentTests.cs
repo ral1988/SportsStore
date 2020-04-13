b8f50f467505006d26563e94c131390421884905
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-04-13 11:54:05 -0400

    Buttons can now be highlighted on the side navbar category section

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+//using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Moq;
 using SportsStore.Components;
 using SportsStore.Models;
 using Xunit;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SportsStore.Tests {
     public class NavigationMenuViewComponentTests {
@@ -26,6 +29,30 @@ namespace SportsStore.Tests {
 // Assert
             Assert.True(Enumerable.SequenceEqual(new string[] { "Apples",
                 "Oranges", "Plums" }, results));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category() {
+// Arrange
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductId = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductId = 4, Name = "P2", Category = "Oranges"},
+            }).AsQueryable<Product>());
+            NavigationMenuViewComponent target =
+                new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext {
+                ViewContext = new ViewContext {
+                    RouteData = new RouteData()
+                }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
+// Action
+            string result = (string)(target.Invoke() as
+                ViewViewComponentResult).ViewData["SelectedCategory"];
+// Assert
+            Assert.Equal(categoryToSelect, result);
         }
     }
 }

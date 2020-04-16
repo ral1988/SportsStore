624d0c467d5534f0af73f5601a656da244052efe
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-04-16 11:53:33 -0400

    Created a controller and view model for the cart

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Infrastructure;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
 
 namespace SportsStore.Controllers 
 {
@@ -12,6 +14,15 @@ namespace SportsStore.Controllers
         public CartController(IProductRepository repo) 
         {
             repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
         }
         public RedirectToActionResult AddToCart(int productId, string returnUrl) 
         {
